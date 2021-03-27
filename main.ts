7db512e7b3bfde147ac5418601fb071067b191a8
Suzu Tomo <47011206+kamekyame@users.noreply.github.com>
2021-03-27 18:28:45 +0900

    first commit

---
+++ main.ts
@@ -0,0 +1,40 @@
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+
+import { pathResolver } from "https://kamekyame.github.io/deno_tools/path/mod.ts";
+const resolve = pathResolver(import.meta);
+
+import { getBearerToken } from "https://kamekyame.github.io/twitter_api_client/auth/oauth2.ts";
+import {
+  connectStream,
+} from "https://kamekyame.github.io/twitter_api_client/api_v2/tweets/filtered_stream.ts";
+
+import { Fortune } from "../fortune-bot/mod.ts";
+
+const env = config({
+  path: resolve("./.env"),
+  safe: true,
+  example: resolve("./.env.example"),
+});
+
+const auth = {
+  consumerKey: env["API_KEY"],
+  consumerSecret: env["API_SECRETKEY"],
+  token: env["TOKEN"],
+  tokenSecret: env["TOKEN_SECRET"],
+};
+
+const bearerToken = await getBearerToken(auth.consumerKey, auth.consumerSecret);
+
+const fortune = new Fortune(auth, bearerToken);
+await fortune.checkRule();
+
+connectStream(bearerToken, (res) => {
+  fortune.callback(res);
+}, {
+  expansions: {
+    author_id: true,
+  },
+  "user.fields": {
+    username: true,
+  },
+});
